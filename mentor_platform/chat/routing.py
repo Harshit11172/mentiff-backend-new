36f8ea352209a1fa64551c5bf745c8f9cd56470a
HarshitPathak <your.harshitkh9@gmail.com>
2025-06-08 01:32:00 +0530

    corrected ws path

--- mentor_platform/chat/routing.py
+++ mentor_platform/chat/routing.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/chat/group/<str:group_name>/', consumers.GroupChatConsumer.as_asgi()),
-    path('ws/chat/private/<str:user_id>/', consumers.PrivateChatConsumer.as_asgi()),
+    path('api_backend/ws/chat/group/<str:group_name>/', consumers.GroupChatConsumer.as_asgi()),
+    path('api_backend/ws/chat/private/<str:user_id>/', consumers.PrivateChatConsumer.as_asgi()),
 ]
--- mentor_platform/voice_video/routing.py
+++ mentor_platform/voice_video/routing.py
@@ -3,6 +3,6 @@ from . import consumers
 from django.urls import path
 
 websocket_urlpatterns = [
-    # re_path(r'ws/call/(?P<call_id>\w+)/$', consumers.CallConsumer.as_asgi()),
-    path('ws/call/<str:call_id>/', consumers.CallConsumer.as_asgi()),
+    # re_path(r'api_backend/ws/call/(?P<call_id>\w+)/$', consumers.CallConsumer.as_asgi()),
+    path('api_backend/ws/call/<str:call_id>/', consumers.CallConsumer.as_asgi()),
 ]
